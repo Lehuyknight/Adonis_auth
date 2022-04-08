e1b63edd7cb2f9c1646baae9154703074e87489c
Lê Huy Knight <loverisaliar@gmail.com>
2022-04-08 15:59:43 +0700

    init project, create sample controller

---
+++ app/Controllers/Http/SitesController.ts
@@ -0,0 +1,7 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class SitesController {
+    public async index() {
+        return "Post controller test"
+    }
+}
